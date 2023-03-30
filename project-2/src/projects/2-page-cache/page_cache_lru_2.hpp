e689a5eebf3fa72fdcd700129ab1e1d455349c3d
Boyan Sun <bsun68@wisc.edu>
2023-03-30 16:23:44 -0500

    set up LRU2 header

--- project-2/src/projects/2-page-cache/page_cache_lru_2.hpp
+++ project-2/src/projects/2-page-cache/page_cache_lru_2.hpp
@@ -2,10 +2,15 @@
 #define CS564_PROJECT_PAGE_CACHE_LRU_2_HPP
 
 #include "page_cache.hpp"
+#include <unordered_map>
+#include <queue>
+#include <algorithm>
 
 class LRU2ReplacementPageCache : public PageCache {
 public:
   LRU2ReplacementPageCache(int pageSize, int extraSize);
+
+  ~LRU2ReplacementPageCache();
 
   void setMaxNumPages(int maxNumPages) override;
 
@@ -20,7 +25,16 @@ public:
   void discardPages(unsigned int pageIdLimit) override;
 
 private:
-  // TODO: Declare class members as needed.
+    struct LRU2ReplacementPage : public Page {
+    LRU2ReplacementPage(int pageSize, int extraSize, unsigned pageId,
+                          bool pinned);
+
+    unsigned pageId;
+    bool pinned;
+  };
+
+  std::unordered_map<unsigned, LRU2ReplacementPage *> pages_;
+  std::vector<unsigned> freePageIDList;
 };
 
 #endif // CS564_PROJECT_PAGE_CACHE_LRU_2_HPP
